84502bde00a3f99eb770e5cd95dae2564b7a6554
oyunsah <guyruli@outlook.com>
2015-06-19 01:36:08 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "OYUNSAH.COM",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
